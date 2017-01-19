d4320e5ffd2bd5ed62bb039211ad68f0d7646f0f
VinodKumar-b <vinodkumar@testings-MacBook-Pro.local>
2017-01-19 01:47:37 -0800

    fancy editing

---
+++ Bin-Social/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Bin-Social
+//
+//  Created by Vinod Kumar on 1/19/17.
+//  Copyright © 2017 self. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
