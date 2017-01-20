9a8a3c12330a2602880a9b899060ba98ef31e75c
VinodKumar-b <vinodkumar@testings-MacBook-Pro.local>
2017-01-20 15:12:44 -0800

    auth fb, fb

--- Bin-Social/SignInVC.swift
+++ Bin-Social/SignInVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
@@ -21,6 +23,33 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtn(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Vinnu: Authentication is Faild due to \(error)")
+            } else if result?.isCancelled == true {
+                print("Vinnu: Authentication is cancelled by user")
+            } else {
+                print("Vinnu: Successfully authenticated")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Vinnu: Firebase Authentication is faild due to \(error)")
+            } else {
+                print("Vinnu: Firebase is successfully authenticated")
+            }
+        })
+    }
 
 }
 
